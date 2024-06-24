58e0ed4a1099ef725479da835db5ea39a11b7a58
Mikolaj Mroszczak <mikolaj.mroszczak2@faculty.ai>
2024-06-24 15:03:19 +0100

    ex 3.14 done

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 const morganLogBody = (morgan.token('body', request => (JSON.stringify(request.body))))
 
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({})
+    .then(persons => {
+        response.json(persons)
+    })
     morganLogBody()
 })
 
@@ -71,27 +52,26 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if(persons.some(person => (person.name === body.name))){
-        return response.status(403).json({
-            error: "name already in the phonebook"
-        })
-    }else{if(!(body.number && body.name)){
-        return response.status(400).json({
-            error: "name or number not specified"
-        })
-    }}
+    // if(persons.some(person => (person.name === body.name))){
+    //     return response.status(403).json({
+    //         error: "name already in the phonebook"
+    //     })
+    // }else{if(!(body.number && body.name)){
+    //     return response.status(400).json({
+    //         error: "name or number not specified"
+    //     })
+    // }}
 
-    const id = Math.floor(Math.random() * 10000000)
-    const person = {
-        id: id,
+    console.log(body)
+
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    console.log(person)
-    persons = persons.concat(person)
-    console.log(persons)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
     morganLogBody()
 })
 
